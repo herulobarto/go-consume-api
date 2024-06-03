c0666a3951ded98f31bd86beb4e30da8dedb736b
herulobarto <herulobarto@gmail.com>
2024-06-03 20:52:52 +0700

    menambah func store

--- controllers/post.go
+++ controllers/post.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"bytes"
 	"encoding/json"
 	"html/template"
 	"log"
@@ -43,9 +44,52 @@ func Index(w http.ResponseWriter, r *http.Request) {
 
 func Create(w http.ResponseWriter, r *http.Request) {
 
+	temp, _ := template.ParseFiles("views/create.html")
+	temp.Execute(w, nil)
+
 }
 
 func Store(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+
+	newPost := Post{
+		Id:     0,
+		Title:  r.Form.Get("post_title"),
+		Body:   r.Form.Get("post_body"),
+		UserId: 1,
+	}
+
+	JsonValue, _ := json.Marshal(newPost)
+	buff := bytes.NewBuffer(JsonValue)
+
+	req, err := http.NewRequest(http.MethodPost, BASE_URL+"/posts", buff)
+	if err != nil {
+		log.Print(err)
+	}
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	httpClient := &http.Client{}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		log.Print(err)
+	}
+	defer res.Body.Close()
+
+	var postResponse Post
+
+	decoder := json.NewDecoder(res.Body)
+	if err := decoder.Decode(&postResponse); err != nil {
+		log.Print(err)
+	}
+
+	// fmt.Println(res.StatusCode)
+	// fmt.Println(res.Status)
+	// fmt.Println(postResponse)
+
+	if res.StatusCode == 201 {
+		http.Redirect(w, r, "/posts", http.StatusSeeOther)
+	}
 
 }
 
